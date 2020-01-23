a4ed0a089e3f840b7c4774bf7038660c842f9780
MacStudent <macstudent@128-iMAC06.local>
2020-01-22 19:46:25 -0500

    Initial commit

---
+++ src/main/java/com/lambtom/firstexample.java
@@ -0,0 +1,32 @@
+package com.lambtom;
+
+public class firstexample {
+    public static void main(String[] args) {
+        System.out.println("welcome to JAVA programming");
+        for (int i = 0; i < 0; i++) {
+            System.out.println("geet");
+        }
+        int a = 100;
+        int b = 200;
+        int c = a + b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println("sum =" + (a + b));
+        System.out.println(a + b + "sum");
+
+        short x = 32767;
+        System.out.println("short MAX =" + Short.MAX_VALUE);
+        System.out.println("short MIN =" + Short.MIN_VALUE);
+
+        long y = 2147483647;
+        System.out.println(y);
+
+        float f1 = 1000.50F;
+        double f2 = 1000.40D;
+        System.out.println("F1 =" + f1);
+        System.out.println("F2 =" + f2);
+
+
+    }
+}
